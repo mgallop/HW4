38fffc4db8fd88e5ad4b1dedcafee380f625ce87
mgallop <max.gallop@gmail.com>
2013-02-09 20:54:42 -0500

    Have a way to change the next value for a given next value, all adds work

--- hw4.py
+++ hw4.py
@@ -9,13 +9,26 @@ class LinkedList:
   def __init__(self, value):
     self.head = Node(value, None)
   def addNode(self, new_value):
-    self.nextChange(None, Node(new_value), self.head)
+    nextChange(None, Node(new_value), self.head)
   def addNodeAfter(self, new_value, after_node):
-    after_node.next = Node(new_value, after_node.next)
+    nextChange(after_node.next, Node(new_value, after_node.next), self.head)
   def addNodeBefore(self, new_value, before_node):
-    self.nextChange(before_node, Node(new_value, before_node), self.head)
-  def nextChange(self, old, new, start):
-    if start.next == old:
-      start.next = new
-    else:
-      nextChange(self, old, new, start.next)
+    nextChange(before_node, Node(new_value, before_node), self.head)
+  
+def nextChange(old, new, start):
+  if NodeEqual(old, start.next):
+    start.next = new
+  else:
+    nextChange(old, new, start.next)
+
+def NodeEqual(node1, node2):
+  if node1 == node2 == None: 
+    return True
+  elif node1 == None or node2 == None:
+    return False
+  elif node1.value == node2.value and node1.next == node2.next == None:
+    return True
+  elif node1.value == node2.value and node1.next.value == node2.next.value:
+    return True
+  else:
+    return False
